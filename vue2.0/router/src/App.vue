8149ac2c2467775f694e1361c13d363c8f7941a5
blackstone86 <huangjunwen@huangjunwendeMacBook-Pro.local>
2017-05-26 17:49:17 +0800

    [App.vue] 命名路由

--- vue2.0/router/src/App.vue
+++ vue2.0/router/src/App.vue
@@ -5,7 +5,7 @@
             <li><router-link to="/foo">Go to Foo</router-link></li>
             <li><router-link to="/bar">Go to Bar</router-link></li>
             <li><router-link to="/user/1">Go to User 1</router-link></li>
-            <li><router-link to="/user/2">Go to User 2</router-link></li>
+            <li><router-link :to="{ name: 'user', params: { id: 2 }}">Go to User 2</router-link></li>
         </ul>
         <router-view></router-view>
     </div>
