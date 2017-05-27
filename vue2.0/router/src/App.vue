269203630550d7c6a0565909dbb64649d2c37579
blackstone86 <huangjunwen@huangjunwendeMacBook-Pro.local>
2017-05-27 10:04:02 +0800

    [App.vue] 命名视图

--- vue2.0/router/src/App.vue
+++ vue2.0/router/src/App.vue
@@ -6,7 +6,10 @@
             <li><router-link to="/bar">Go to Bar</router-link></li>
             <li><router-link to="/user/1">Go to User 1</router-link></li>
             <li><router-link :to="{ name: 'user', params: { id: 2 }}">Go to User 2</router-link></li>
+            <li><router-link to="/views">Go to Views</router-link></li>
         </ul>
         <router-view></router-view>
+        <router-view name="view_2"></router-view>
+        <router-view name="view_3"></router-view>
     </div>
 </template>
